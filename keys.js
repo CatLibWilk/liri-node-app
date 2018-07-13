231554bdd5451bdba97c8c2d5b4d8e7409ee5c9c
Andy Wilk <andrewdwilk@gmail.com>
2018-07-13 13:53:11 -0400

    keys working, initial switch and functions setup

--- keys.js
+++ keys.js
@@ -5,6 +5,10 @@ var TWITTER_CONSUMER_SECRET = "Dqx2DzTNAwhAyijdDOH5irK1nvHpXXP9sEcPlZ3d9MHugSOzz
 var TWITTER_ACCESS_TOKEN_KEY = "2890108565-bYkbUHoz2wpz5eDKrMqEw5YmruyvqPt8TBZouVS";
 var TWITTER_ACCESS_TOKEN_SECRET = "NDRD9sap4YoboUmplYfPKBHK85HX5mX1Sb6jCxRRIRrZU";
 
+var SPOTIFY_ID = "4de666d2c18c4904808211e81e328d8a";
+var SPOTIFY_SECRET = "dc11eaafd2ae4a129012586f0cace1eb";
+
+
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
--- liri.js
+++ liri.js
@@ -2,8 +2,52 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var service = process.argv[2];
+var input = process.argv[3];
 
-console.log(client);
+
+////interprets the service bein requested//////
+switch(service) {
+    case "my-tweets":
+        tweet();
+        break;
+
+    case "spotify-this-song":
+        spotifySong();
+        break;
+
+    case "movie-this":
+        movie();
+        break;
+
+    case "do-what-it-says":
+        doSay();
+        break;
+
+    
+    default:
+        console.log("please select one of the services to use.")
+}
+
+////servicefunctions defined/////////////
+
+
+function tweet(){
+    console.log("tweet function was called");
+}
+
+function spotifySong(){
+    console.log("spotify function was called");
+}
+
+function movie(){
+    console.log("movie function was called");
+}
+
+function doSay(){
+    console.log("doSay function was called");
+}
