bed0dadf0b66297541e7a4962daa3bcc4dfa07e4
Andy Wilk <andrewdwilk@gmail.com>
2018-07-13 16:14:41 -0400

    spotify search working except for issue with default search

--- liri.js
+++ liri.js
@@ -65,7 +65,33 @@ function tweet(){
 }
 
 function spotifySong(){
-    console.log("spotify function was called");
+    var searchInput = "";
+    if(input != null){
+        searchInput = input;
+    }else{
+        searchInput = "The Sign"
+    }
+    spotify.search({ type: 'track', query: searchInput, limit:1}, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        console.log(input);
+        var artist = (data.tracks.items[0].artists[0].name);
+        console.log("Artist: ", artist);
+
+        var song = data.tracks.items[0];
+        console.log("Song Title: ", song.name);
+
+        var album = data.tracks.items[0].album.name;
+        console.log("Album title: ", album);
+
+        var preview = data.tracks.items[0].preview_url;
+        console.log("Listen to a preview here: ", preview);
+    });
+    
+        
+    
 }
 
 function movie(){
@@ -75,3 +101,5 @@ function movie(){
 function doSay(){
     console.log("doSay function was called");
 }
+
+console.log("input was", input);
